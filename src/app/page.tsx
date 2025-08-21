1c00cc0ad3cd2bf60507bb9a64590e4a52fcb5ac
hyomin <miamiq0000@gmail.com>
2025-08-21 09:13:39 +0900

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,51 +11,8 @@ import SubHeroSection from "@/components/sections/sub-hero-section/sub-hero-sect
 import ValuePickSection from "@/components/sections/value-pick-section/value-pick-section";
 import ValueShareSection from "@/components/sections/value-share-section/value-share-section";
 import ValueTalkSection from "@/components/sections/value-talk-section/value-talk-section";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
 
 export default function Page() {
-  const [authorized, setAuthorized] = useState(false);
-  const [password, setPassword] = useState("");
-
-  if (!authorized) {
-    return (
-      <div
-        style={{
-          minHeight: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <input
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            if (password === "0414") setAuthorized(true);
-            else alert("비밀번호가 틀렸습니다.");
-          }
-        }}
-          autoFocus
-          type="password"
-          value={password}
-          className="text-center border rounded-lg"
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="비밀번호"
-          style={{ padding: "8px", fontSize: "16px", marginBottom: "12px" }}
-        />
-        <Button
-          onClick={() => {
-            if (password === "0414") setAuthorized(true);
-            else alert("비밀번호가 틀렸습니다.");
-          }}
-          style={{ padding: "8px 16px", fontSize: "16px" }}
-        >
-          확인
-        </Button>
-      </div>
-    );
-  }
   return (
     <div className="relative">
       <Header />
