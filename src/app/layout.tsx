fa4ed911adc4a9ba1e0c2877c6d2fcd8df717cdb
hyomin <miamiq0000@gmail.com>
2025-08-22 22:04:10 +0900

    x

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-08F110M34C" />
     </html>
   );
 }
